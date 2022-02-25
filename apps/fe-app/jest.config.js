41b29392a66b7c753da2f0c987a1c3fa624867e9
Luca Del Puppo <Puppo@users.noreply.github.com>
2022-02-25 09:28:05 +0100

    First App (#1)

---
+++ apps/fe-app/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'fe-app',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  transform: {
+    '^.+\\.[tj]s$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/fe-app',
+};
